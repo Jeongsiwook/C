d740fefa7cad36826601e14dfd49a56ddc26281f
Jeongsiwook <jsu0638@naver.com>
2020-09-23 18:59:43 +0900

    Rename 03-01_삼각형그리기.c to 03_01_삼각형그리기.c

--- practice/03_01_삼각형그리기.c
+++ practice/03_01_삼각형그리기.c
@@ -1,4 +1,4 @@
-// "*"�� �̿��� �ﰢ�� �׸��׸���
+// "*"를 이용해 삼각형 그림그리기
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -8,15 +8,15 @@ int main()
     int num;
     scanf("%d", &num);
     if (num > 30) {
-        printf("�ʹ� Ů�ϴ�.");
+        printf("너무 큽니다.");
         return 0;
     }
 
     for (int i = 0; i < num; i++) {
-        for (int j = 0; j < num - 1 - i; j++) { // ��ĭ �κ�
+        for (int j = 0; j < num - 1 - i; j++) { // 빈칸 부분
             printf(" ");
         }
-        for (int j = 0; j < (2 * i + 1); j++) { // * �κ�
+        for (int j = 0; j < (2 * i + 1); j++) { // * 부분
             printf("*");
         }
         printf("\n");
